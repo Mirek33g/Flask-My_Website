f56f85b9cf459d0de1dc0fbacace59449fcfc61e
mirek <mirek33g@gmail.com>
2024-02-22 11:41:47 +0000

    social media fixed

--- main.py
+++ main.py
@@ -5,26 +5,23 @@ app = Flask(__name__)
 year = datetime.datetime.now().year
 
 
-
-
 @app.route('/')
 def home():
     return render_template('home_page.html', year=year)
 
 
-@app.route('/<web>', methods=['GET', 'POST'])
+@app.route('/<web>')
 def get_web(web):
-    if request.method == 'POST':
-        if web == 'facebook':
-            return redirect('https:/facebook.com/mirek.gregorowicz')
-        elif web == 'github':
-            return redirect('https://github.com/Mirek33g')
-        elif web == 'twitter':
-            return redirect('https://twitter.com/mirekgregorowicz')
-        elif web == 'email':
-            return redirect('mailto: email@gmail.com')
-        else:
-            return redirect('/')
+    if web == 'twitter':
+        return redirect('https://twitter.com/Mirek4g')
+    elif web == 'facebook':
+        return redirect('https://www.facebook.com/mirek.gregorowicz')
+    elif web == 'github':
+        return redirect('https://github.com/Mirek33g')
+    elif web == 'linkedin':
+        return redirect('https://www.linkedin.com/in/miroslaw-gregorowicz-29694a29a/')
+    elif web == 'email':
+        return redirect('mailto: mirek33g@gmail.com')
     else:
         return redirect('/')
 
@@ -41,3 +38,5 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
